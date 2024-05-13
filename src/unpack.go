55f12a9c4dea4bf8bb3e5406a1ca6b06e8f7b201
Hakan Okay <hokay.ca@gmail.com>
2024-05-13 13:04:16 -0400

    Make UnpackedBytes fields names consistent

--- src/unpack.go
+++ src/unpack.go
@@ -1,12 +1,12 @@
 package unpack
 
 type UnpackedBytes struct {
-	Integer           int32   `json:"integer,omitempty"`
-	UnsignedInteger   uint32  `json:"unsigned_integer,omitempty"`
-	Short             int16   `json:"short,omitempty"`
-	Float             float32 `json:"float,omitempty"`
-	Double            float32 `json:"double,omitempty"`
-	Big_endian_double float32 `json:"big___endian___double,omitempty"`
+	Integer         int32   `json:"integer,omitempty"`
+	UnsignedInteger uint32  `json:"unsigned_integer,omitempty"`
+	Short           int16   `json:"short,omitempty"`
+	Float           float32 `json:"float,omitempty"`
+	Double          float32 `json:"double,omitempty"`
+	BigEndianDouble float32 `json:"big___endian___double,omitempty"`
 }
 
 func Unpack(s string) UnpackedBytes {
