ca7bd62707305c973057b62c67426c45511969ff
Hakan Okay <hokay.ca@gmail.com>
2024-05-13 14:49:06 -0400

    Update encode/decode data types

--- src/encoding.go
+++ src/encoding.go
@@ -2,17 +2,39 @@ package unpack
 
 import (
 	"encoding/base64"
+	"errors"
+	"fmt"
 	logger "help_me_unpack/log"
 )
 
-func Encode(s string) string {
-	return base64.StdEncoding.EncodeToString([]byte(s))
+var EncodeInvalidErr = "Encode received no bytes\n"
+var DecodeInvalidErr = "Decode received no/invalid bytes\n"
+var EncodeParseErr = "Couldn't encode\n"
+var DecodeParseErr = "Couldn't decode: %v\n"
+
+func Encode(b []byte) ([]byte, error) {
+	if b == nil {
+		return nil, errors.New(EncodeInvalidErr)
+	}
+
+	encoded := base64.StdEncoding.EncodeToString(b)
+	if len(encoded) == 0 {
+		return nil, errors.New(EncodeParseErr)
+	}
+
+	return []byte(encoded), nil
 }
 
-func Decode(s string) string {
-	decoded, err := base64.StdEncoding.DecodeString(s)
-	if err != nil {
-		logger.Printf("Coulnd't decode base64", logger.ERROR, err.Error())
+func Decode(b []byte) ([]byte, error) {
+	if b == nil {
+		return nil, errors.New(DecodeInvalidErr)
 	}
-	return string(decoded)
+
+	decoded, err := base64.StdEncoding.DecodeString(string(b))
+	if err != nil {
+		return nil, fmt.Errorf(DecodeParseErr, decoded)
+	}
+
+	logger.Printf("Decode wrote %d bytes\n", logger.INFO, len(decoded))
+	return []byte(decoded), nil
 }
